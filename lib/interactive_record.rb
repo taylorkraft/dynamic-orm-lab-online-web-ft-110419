566ad20615302a79a47e5e929e30a3721c105514
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-29 01:12:36 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,16 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row['name']
+    end
+    column_names.compact
+  end
 end
