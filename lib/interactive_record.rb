db1820d7972efedb50e6e0d925fd8db44b265e76
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-29 03:47:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,7 +43,7 @@ class InteractiveRecord
   end
 
   def save
-    sql = "INSERT into #{table_name_for_insert} (#{col_names_for_insert}) VALUES #{values_for_insert})"
+    sql = "INSERT into #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
