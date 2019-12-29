7bdb127ba8ac0583df0b5bcac216cf162a684504
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-29 05:04:57 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,6 +55,6 @@ class InteractiveRecord
 
   def self.find_by(attribute_hash)
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys[0].to_s}= '#{attribute_hash.values[0]}.to_s'"
-    row = DB[:conn].execute(sql)
+    DB[:conn].execute(sql)
   end
 end
